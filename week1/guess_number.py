8f1fff9257024ca6f9fad42b81a2b59811359cd8
GGBOY626 <z1596761805@gmail.com>
2025-08-09 09:32:38 +1200

    guess_number change2

--- week1/guess_number.py
+++ week1/guess_number.py
@@ -5,18 +5,22 @@ def guess_number():
     print(words)
     guess_words = [''] * len(words)
     life = len(words) *2
-    print(guess_words)
+    print("word you need guess:",guess_words)
     while life > 0 and guess_words != list(words):
         word = input("please input:")
         if words.__contains__(word):
             life = life -1
             guess_words[words.index(word)] = word
-            print("words:", words)
             print("guess right! you life rest", life)
+            print("word you need continue to guess:",guess_words)
             print (guess_words)
         else:
             life = life -1
             print("guess wrong you life rest",life)
+    if guess_words == list(words):
+        print("congratulation you win")
+    else:
+        print("you lost")
 
 
 guess_number()
