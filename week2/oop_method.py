bdfa331a8570ffa998abc31d7a0611b34d612fe3
GGBOY626 <z1596761805@gmail.com>
2025-08-10 11:42:00 +1200

    oop

--- week2/oop_method.py
+++ week2/oop_method.py
@@ -1,26 +1,23 @@
 class Open:
-    def __init__(self, words):
-        self.words = words
 
-    def search(self, char):
-        position = self.words.find(char)
+    def search(self,words, char):
+        position = words.find(char)
         if position != -1:
             print(f"char '{char}' in the position: {position}")
         else:
             print(f"char '{char}' not found")
 
-    def print_length(self):
-        print(f"words length: {len(self.words)}")
+    def print_length(self,words):
+        print(f"words length: {len(words)}")
 
-    def print_uppercase(self):
-        print(f"upper words: {self.words.upper()}")
+    def print_uppercase(self,words):
+        print(f"upper words: {words.upper()}")
 
 
 if __name__ == "__main__":
     words = input("input words: ")
     char = input("input search char: ")
-
-    obj = Open(words)
-    obj.search(char)
-    obj.print_length()
-    obj.print_uppercase()
+    name = Open()
+    name.search(words,char)
+    name.print_length(words)
+    name.print_uppercase(words)
