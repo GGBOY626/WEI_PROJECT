0339bb6a0dfa13057948e853c1a1d0ebb1b1754e
GGBOY626 <z1596761805@gmail.com>
2025-08-16 09:05:23 +1200

    File process

--- week3/file_read.py
+++ week3/file_read.py
@@ -1,6 +1,14 @@
-read_file = open("D:/black/demo.txt","r",encoding="UTF-8")
-print(read_file.read())
-read_file.close()
-append_file = open("D:/black/demo.txt","a",encoding="UTF-8")
-append_file.write("hello world")
-append_file.close()
+class FileProcess:
+
+    def read_file(path):
+        read_file = open(path,"r",encoding="UTF-8")
+        print(read_file.read())
+        read_file.close()
+
+    def write_file(path,content):
+        append_file = open(path,"a",encoding="UTF-8")
+        append_file.write(content)
+        append_file.close()
+if __name__ == '__main__':
+    FileProcess.read_file("D:/black/demo.txt")
+    FileProcess.write_file("D:/black/demo.txt","\n hello world")
