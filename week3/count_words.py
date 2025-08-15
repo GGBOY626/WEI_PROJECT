5a194005e0f3dbe8fbe58a192ae496be05ba3811
GGBOY626 <z1596761805@gmail.com>
2025-08-16 09:25:08 +1200

    count_result

---
+++ week3/count_words.py
@@ -0,0 +1,12 @@
+class CountWords:
+    def __init__(self, path):
+        self.path = path
+
+    def count_words(self):
+        read_file = open(self.path, "r", encoding="UTF-8")
+        text = read_file.read()
+        result = len(text.split())
+        return result
+if __name__ == '__main__':
+    fp = CountWords("D:/black/demo.txt")
+    print(fp.count_words())
