8e07e378229a1b881a9005fb6d57700f53f19e21
GGBOY626 <z1596761805@gmail.com>
2025-08-16 09:08:30 +1200

    OOP

--- week3/file_read.py
+++ week3/file_read.py
@@ -1,14 +1,16 @@
 class FileProcess:
-
-    def read_file(path):
-        read_file = open(path,"r",encoding="UTF-8")
+    def __init__(self, path):
+        self.path = path
+    def read_file(self):
+        read_file = open(self.path,"r",encoding="UTF-8")
         print(read_file.read())
         read_file.close()
 
-    def write_file(path,content):
-        append_file = open(path,"a",encoding="UTF-8")
+    def write_file(self,content):
+        append_file = open(self.path,"a",encoding="UTF-8")
         append_file.write(content)
         append_file.close()
 if __name__ == '__main__':
-    FileProcess.read_file("D:/black/demo.txt")
-    FileProcess.write_file("D:/black/demo.txt","\n hello world")
+    fp = FileProcess("D:/black/demo.txt")
+    fp.read_file()
+    fp.write_file("\n hello world")
