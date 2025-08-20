5f1ca66c0380d7cb19fc42d6edd01229bc58bdd4
WEI ZHENG <z1596761805@gmail.com>
2025-08-20 13:42:16 +1200

    parquet

--- week3/parquet_format.py
+++ week3/parquet_format.py
@@ -1,5 +1,3 @@
-import os
-
 import pandas as pd
 
 class ParquetFormat:
@@ -7,35 +5,39 @@ class ParquetFormat:
         self.input_csv_path = input_csv_path
         self.output_parquet_path = output_parquet_path
         self.stats_output_csv_path = stats_output_csv_path
-        self.df = None
 
     def read_and_convert_to_parquet(self):
-        df = pd.read_csv(self.input_csv_path, encoding='UTF-8')
-        df.to_parquet(self.output_parquet_path, index=False)
+        df = pd.read_csv(
+            self.input_csv_path,
+            sep=';',
+            encoding='utf-8',
+            na_values=['::', ':', '']
+        )
+        df.to_parquet(self.output_parquet_path, index=False, engine='pyarrow')
 
     def compute_stats(self):
-        # input Parquet
-        df = pd.read_parquet(self.parquet_path, engine='pyarrow')
+        df = pd.read_parquet(self.output_parquet_path, engine='pyarrow')
         numeric_df = df.select_dtypes(include=['number'])
-
-        # calculate
         stats = pd.DataFrame({
-            "max": numeric_df.max(),
-            "min": numeric_df.min(),
-            "mean": numeric_df.mean(),
-            "abs_max": numeric_df.abs().max(),
-            "abs_min": numeric_df.abs().min(),
-            "abs_mean": numeric_df.abs().mean()
+            "max": numeric_df.max(numeric_only=True),
+            "min": numeric_df.min(numeric_only=True),
+            "mean": numeric_df.mean(numeric_only=True),
+            "abs_max": numeric_df.abs().max(numeric_only=True),
+            "abs_min": numeric_df.abs().min(numeric_only=True),
+            "abs_mean": numeric_df.abs().mean(numeric_only=True),
         })
 
-        # save to csv
-        stats_path = os.path.splitext(self.parquet_path)[0] + '_stats.csv'
-        stats.to_csv(stats_path)
+        stats.to_csv(self.stats_output_csv_path, encoding='utf-8')
 
-        print(f"finish：{stats_path}")
+        print(f"finish: {self.stats_output_csv_path}")
         print(stats)
+        return stats
 
 if __name__ == '__main__':
-     obj = ParquetFormat("D:/black/AirQualityUCI.csv","D:/class_project/personnel_develop/test01/week3/data.parquet","D:/black/result.csv")
-     # obj.read_csv()
-     obj.compute_stats()
+    obj = ParquetFormat(
+        "D:/black/AirQualityUCI.csv",
+        "D:/class_project/personnel_develop/test01/week3/data.parquet",
+        "D:/black/result.csv"
+    )
+    obj.read_and_convert_to_parquet()
+    obj.compute_stats()
