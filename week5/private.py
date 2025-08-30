fa83090eee4703a8eb862c3cfa02ad3c00b76783
WEI ZHENG <z1596761805@gmail.com>
2025-08-31 11:14:19 +1200

    update

--- week5/private.py
+++ week5/private.py
@@ -8,10 +8,20 @@ class Teacher:
 
     def speak2(self):
         print('teacher bank_account is ' + self.__bank_account)
-
-
+class Student:
+    def __init__(self, name, visa_card, bank_account):
+        self.name = name
+        self._visa_card = visa_card
+        self.__bank_account = bank_account
+    def show(self):
+        # public
+        print("Student can access public name:", self.name)
+        # protect
+        print("Student can access protected visa_card:", self._visa_card)
 if __name__ == '__main__':
     t = Teacher("Mike", "VISA-123", "BANK-456")
     t.speak()
     t.speak2()
+    s = Student("Tom", "VISA-789", "BANK-999")
+    s.show()
 
