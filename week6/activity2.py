7dde91560c9235e2515dfb2ea03a6d9de918bc9e
WEI ZHENG <z1596761805@gmail.com>
2025-09-07 11:15:16 +1200

    update

--- week6/activity2.py
+++ week6/activity2.py
@@ -1,19 +1,6 @@
-# use enumerate() for looping to add 5 extra point to each grade in the list, the 5th one add 10
-grades = [88, 92, 78, 65, 50, 94]
-for i,grade in enumerate(grades):
-    if i == 4:
-        grades[i] += 10
-    else:
-        grades[i] += 5
-
-# filter out elements depend on their index:
-# use list comprehension and enumerate() to get elements with even index
-data = [100, 200, 300, 400, 500]
-result = [v for i,v in enumerate(data) if i%2==0]
-print(result)
-
-# create a dictionary from lists using zip()
-keys = ['name', 'age', 'grade']
-values = ['Alice', 25, 'A']
-result2 = dict(zip(keys,values))
-print(result2)
+data = ['a5', 'a2', 'b1', 'b3', 'c2']
+sorted_data = sorted(data, key=lambda x: (x[0], int(x[1:])))
+print(sorted_data)
+# explain:['a2', 'a5', 'b1', 'b3', 'c2']
+# because use x[0] to sort, is use ascil code
+# if ascil code the same will sort by number
